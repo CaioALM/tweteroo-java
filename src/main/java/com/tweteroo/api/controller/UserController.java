84058c3b2507fba4577a3010a66ff36e5586496a
Caio <caiosobreira77@gmail.com>
2023-01-22 01:12:39 -0300

    chore: add user router

--- src/main/java/com/tweteroo/api/controller/UserController.java
+++ src/main/java/com/tweteroo/api/controller/UserController.java
@@ -1,6 +1,21 @@
 package com.tweteroo.api.controller;
 
-public class UserController {
-    
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import com.tweteroo.api.dto.*;
+
+@RestController
+@RequestMapping("/sign-up")
+
+public class UserController {
+
+    @PostMapping
+    public void createUser(@RequestBody UserDTO req) {
+
+        System.out.println(req.username());
+
+    }
 }
